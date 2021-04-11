76b1d28f7ed505070c4a1a47faeac1fb9a86b699
Dmitrj Kotik <k-work31@yandex.ru>
2021-04-11 23:00:54 +0300

    #5 add findBiId and findByTitle methods in tests for SourceRepository

--- src/main/java/news/model/Source.java
+++ src/main/java/news/model/Source.java
@@ -8,15 +8,15 @@ public class Source {
     private String title;
     private String url;
 
-    public Source(int id, String url, String title) {
+    public Source(int id, String title, String url) {
         this.id = id;
         this.url = url;
         this.title = title;
     }
 
-    public Source(String url, String title) {
-        this.url = url;
+    public Source(String title, String url) {
         this.title = title;
+        this.url = url;
     }
 
     /**
