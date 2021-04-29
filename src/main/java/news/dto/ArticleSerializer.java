c0a35eeeaed95cf69ad01af220182844f55b6ee1
Dmitrj Kotik <k-work31@yandex.ru>
2021-04-29 20:42:37 +0300

    #7 rewrite ArticleSerializer and ArticleSerializerTest

--- src/main/java/news/dto/ArticleSerializer.java
+++ src/main/java/news/dto/ArticleSerializer.java
@@ -57,7 +57,7 @@ public class ArticleSerializer implements Serializer<Article> {
 
     @Override
     public Article toObject() {
-        int id;
+        int id = 0;
         String title;
         String lead;
         LocalDate createDate;
@@ -67,41 +67,53 @@ public class ArticleSerializer implements Serializer<Article> {
         int categoryId;
         int userId;
         int sourceId;
+        int indexLine = 1;
+        boolean withId;
 
         String[] lines = json.split("\n");
         /*for (int i = 0; i < lines.length; i++) {
             System.out.println(i + ":" + lines[i]);
         }*/
-
-        // id
-        Pattern p = Pattern.compile(":(\\d+),");
-        Matcher m = p.matcher(lines[1]);
-        m.find();
-        id = Integer.parseInt(m.group(1));
+        // проверка, есть ли id в строке или нет
+        Pattern p = Pattern.compile("\"id\":.+");
+        Matcher m = p.matcher(lines[indexLine]);
+        withId = m.find();
+        if (withId) {
+            p = Pattern.compile(":(\\d+),");
+            m = p.matcher(lines[indexLine]);
+            m.find();
+            id = Integer.parseInt(m.group(1));
+            indexLine++;
+        }
         // title
-        m = Pattern.compile(":\"(.+)\",").matcher(lines[2]);
+        m = Pattern.compile(":\"(.+)\",").matcher(lines[indexLine]); // 1/2
         m.find();
         title = m.group(1);
+        indexLine++;
         // lead
-        m = Pattern.compile(":\"(.+)\",").matcher(lines[3]);
+        m = Pattern.compile(":\"(.+)\",").matcher(lines[indexLine]); // 2/3
         m.find();
         lead = m.group(1);
+        indexLine++;
         // createDate
-        m = Pattern.compile(":(\\d+),").matcher(lines[4]);
+        m = Pattern.compile(":(\\d+),").matcher(lines[indexLine]); // 3/4
         m.find();
         int timestampCreateDate = Integer.parseInt(m.group(1));
         createDate = Timestamp.from(Instant.ofEpochSecond(timestampCreateDate)).toLocalDateTime().toLocalDate();
+        indexLine++;
         // editDate
-        m = Pattern.compile(":(\\d+),").matcher(lines[5]);
+        m = Pattern.compile(":(\\d+),").matcher(lines[indexLine]); // 4/5
         m.find();
         int timestampEditDate = Integer.parseInt(m.group(1));
         editDate = Timestamp.from(Instant.ofEpochSecond(timestampEditDate)).toLocalDateTime().toLocalDate();
+        indexLine++;
         // text
-        m = Pattern.compile(":\"(.+)\",").matcher(lines[6]);
+        m = Pattern.compile(":\"(.+)\",").matcher(lines[indexLine]); // 5/6
         m.find();
         text = m.group(1);
+        indexLine++;
         // isPublished
-        m = Pattern.compile(":(\\w{4,5}),").matcher(lines[7]);
+        m = Pattern.compile(":(\\w{4,5}),").matcher(lines[indexLine]); // 6/7
         if (m.find()) {
             if (m.group(1).equals("true")) {
                 isPublished = true;
@@ -110,57 +122,66 @@ public class ArticleSerializer implements Serializer<Article> {
                 isPublished = false;
             }
         }
+        indexLine++;
         // categoryId
-        m = Pattern.compile(":(\\d+),").matcher(lines[8]);
+        m = Pattern.compile(":(\\d+),").matcher(lines[indexLine]); // 7/8
         m.find();
         categoryId = Integer.parseInt(m.group(1));
+        indexLine++;
         // userId
-        m = Pattern.compile(":(\\d+),").matcher(lines[9]);
+        m = Pattern.compile(":(\\d+),").matcher(lines[indexLine]); // 8/9
         m.find();
         userId = Integer.parseInt(m.group(1));
+        indexLine++;
         // sourceId
-        m = Pattern.compile(":(\\d+),").matcher(lines[10]);
+        m = Pattern.compile(":(\\d+),").matcher(lines[indexLine]); // 9/10
         m.find();
         sourceId = Integer.parseInt(m.group(1));
-
         // создаем по распарсеным данным объект статьи
-        Article article = new Article(id, title, lead, createDate, editDate, text, isPublished, categoryId, userId, sourceId);
+        Article article;
+        if (withId) {
+            article = new Article(id, title, lead, createDate, editDate, text, isPublished, categoryId, userId, sourceId);
+        } else {
+            article = new Article(title, lead, createDate, editDate, text, isPublished, categoryId, userId, sourceId);
+        }
 
         // если прикрепленных изображений нет
-        if (Pattern.compile("]").matcher(lines[12]).find()) {
+        if (Pattern.compile("]").matcher(lines[indexLine + 2]).find()) {
             System.out.println("Прикреплений нет");
             // если не указаны теги
-            if (Pattern.compile("]").matcher(lines[14]).find()) {
+            if (Pattern.compile("]").matcher(lines[indexLine + 4]).find()) {
                 System.out.println("Тегов нет");
             // если теги указаны
             } else {
-                int indexEndTag = 14;
+                int indexEndTag = indexLine + 4;
                 while (true) {
-                    Matcher localMatcher = Pattern.compile(":(\\d+),").matcher(lines[indexEndTag]);
+                    Matcher localMatcher = Pattern.compile("(\\d+)").matcher(lines[indexEndTag]);
                     localMatcher.find();
                     int idTag = Integer.parseInt(m.group(1));
                     article.addNewTagId(idTag);
                     indexEndTag += 1;
-                    if (!Pattern.compile(":(\\d+),").matcher(lines[indexEndTag]).find()) {
+                    if (!Pattern.compile("(\\d+)").matcher(lines[indexEndTag]).find()) {
                         return article;
                     }
                 }
             }
         // если прикрепленные изображения есть
         } else {
-            int indexEndImages = 13;
+            int indexEndImages = indexLine + 3; // 12/13
+
             // извлекаем все изображения
             while (true) {
-                int idImage;
+                int idImage = 0;
                 String titleImage;
                 String pathImage;
                 int articleIdImage;
-
                 // id
-                Matcher m1 = Pattern.compile(":(\\d+),").matcher(lines[indexEndImages]);
-                m1.find();
-                idImage = Integer.parseInt(m1.group(1));
-                indexEndImages += 1;
+                if (withId) {
+                    Matcher m1 = Pattern.compile(":(\\d+),").matcher(lines[indexEndImages]);
+                    m1.find();
+                    idImage = Integer.parseInt(m1.group(1));
+                    indexEndImages += 1;
+                }
 
                 // title
                 Matcher m2 = Pattern.compile(":\"(.+)\",").matcher(lines[indexEndImages]);
@@ -175,14 +196,19 @@ public class ArticleSerializer implements Serializer<Article> {
                 indexEndImages += 1;
 
                 // articleId
-                Matcher m4 = Pattern.compile(":(\\d+),").matcher(lines[indexEndImages]);
+                Matcher m4 = Pattern.compile(":(\\d+)").matcher(lines[indexEndImages]);
                 m4.find();
                 articleIdImage = Integer.parseInt(m4.group(1));
                 indexEndImages += 3;
 
                 // создаем изображение и добавляем в статью
-                Article.ArticleImage articleImage = new Article.ArticleImage(idImage, titleImage, pathImage, articleIdImage);
-                article.addNewImage(articleImage);
+                if (withId) {
+                    Article.ArticleImage articleImage = new Article.ArticleImage(idImage, titleImage, pathImage, articleIdImage);
+                    article.addNewImage(articleImage);
+                } else {
+                    Article.ArticleImage articleImage = new Article.ArticleImage(titleImage, pathImage, articleIdImage);
+                    article.addNewImage(articleImage);
+                }
 
                 if (Pattern.compile("tagsId").matcher(lines[indexEndImages]).find()) {
                     break;
@@ -209,8 +235,14 @@ public class ArticleSerializer implements Serializer<Article> {
     private String serializeTagsIdToJSON(Object tagsId) {
         List tags = new ArrayList((HashSet) tagsId);
         StringBuilder result = new StringBuilder();
-        for (Object tagId : tags) {
-            result.append("\t\t").append(tagId).append(",\n");
+        for (int i = 0; i < tags.size(); i++) {
+            Object tagId = tags.get(i);
+            result.append("\t\t").append(tagId);
+            if (i != tags.size() - 1) {
+                result.append(",\n");
+            } else {
+                result.append("\n");
+            }
         }
         return result.toString();
     }
@@ -218,18 +250,24 @@ public class ArticleSerializer implements Serializer<Article> {
     private String serializeImagesToJSON(String[] imageFields, Object images) {
         List listImageObjects = (ArrayList) images;
         StringBuilder result = new StringBuilder();
-        for (Object imageObject : listImageObjects) {
-            Article.ArticleImage image = (Article.ArticleImage) imageObject;
+        for (int i = 0; i < listImageObjects.size(); i++) {
+            Article.ArticleImage image = (Article.ArticleImage) listImageObjects.get(i);
             Object[] imageInstance = image.getObjects();
             String imageString = "" +
                     "\t\t" + "{\n" +
                     "\t\t\t" + "\"" + imageFields[0] + "\"" + ":" + imageInstance[0] + ",\n" +
                     "\t\t\t" + "\"" + imageFields[1] + "\"" + ":" + "\"" + imageInstance[1] + "\"" + ",\n" +
                     "\t\t\t" + "\"" + imageFields[2] + "\"" + ":" + "\"" + imageInstance[2] + "\"" + ",\n" +
-                    "\t\t\t" + "\"" + imageFields[3] + "\"" + ":" + imageInstance[3] + ",\n" +
-                    "\t\t" + "},\n";
+                    "\t\t\t" + "\"" + imageFields[3] + "\"" + ":" + imageInstance[3] + "\n" +
+                    "\t\t";
+            if (i != listImageObjects.size() - 1) {
+                imageString += "},\n";
+            } else {
+                imageString += "}\n";
+            }
             result.append(imageString);
         }
+
         return result.toString();
     }
 }
