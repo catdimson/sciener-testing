f5cb7fb793801f00ab9601e7f8f7e13bc38c25f4
Dmitrj Kotik <k-work31@yandex.ru>
2021-05-04 04:13:38 +0300

    #7 add PГT and DELETE methods for SourceController

--- src/test/java/news/web/controllers/SourceControllerTest.java
+++ src/test/java/news/web/controllers/SourceControllerTest.java
@@ -248,124 +248,101 @@ class SourceControllerTest {
         soft.assertAll();
     }
 
-//    @Test
-//    void buildResponsePUTMethod() throws IOException, SQLException {
-//        SoftAssertions soft = new SoftAssertions();
-//        Connection connection = this.poolConnection.getConnection();
-//        Statement statement = connection.createStatement();
-//        String sqlInsertUser = String.format("INSERT INTO \"user\" (password, username, first_name, last_name, email, " +
-//                        "last_login, date_joined, is_superuser, is_staff, is_active, group_id) VALUES ('%s', '%s', '%s', '%s', " +
-//                        "'%s', '%s', '%s', %s, %s, %s, %s);", "password1", "username1", "firstname1", "lastname1", "email1@mail.ru",
-//                Timestamp.valueOf(lastLogin.atStartOfDay()), Timestamp.valueOf(dateJoined.atStartOfDay()), true, true, true, 1);
-//        statement.executeUpdate(sqlInsertUser);
-//        User user = new User("qwerty123", "alex1992", "Александр", "Колесников",
-//                "alex1993@mail.ru", lastLogin, dateJoined, true, true, true, 1);
-//
-//        clientSocket = new Socket("127.0.0.1", 5000);
-//        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-//        out = new PrintWriter(new PrintWriter(clientSocket.getOutputStream(), true));
-//        String expectedResult = "" +
-//                "HTTP/1.1 204 Нет данных\n" +
-//                "Cache-Control: no-store, no-cache, must-revalidate\n" +
-//                "Pragma: no-cache\n";
-//
-//        String request = "" +
-//                "PUT /user/1/ HTTP/1.1\n" +
-//                "Accept: application/json, */*; q=0.01\n" +
-//                "Content-Type: application/json\n" +
-//                "Host: 127.0.0.1:5000\n" +
-//                "UnitTest: true\n" +
-//                "UrlPostgres: " + this.container.getJdbcUrl() + "\n" +
-//                "UserPostgres: " + this.container.getUsername() + "\n" +
-//                "PasswordPostgres: " + this.container.getPassword() + "\n" +
-//                "\n" +
-//                "{\n" +
-//                "\t\"id\":1,\n" +
-//                "\t\"password\":\"qwerty123\",\n" +
-//                "\t\"username\":\"alex1992\",\n" +
-//                "\t\"firstName\":\"Александр\",\n" +
-//                "\t\"lastName\":\"Колесников\",\n" +
-//                "\t\"email\":\"alex1993@mail.ru\",\n" +
-//                "\t\"lastLogin\":1589922000,\n" +
-//                "\t\"dateJoined\":1558299600,\n" +
-//                "\t\"isSuperuser\":true,\n" +
-//                "\t\"isStaff\":true,\n" +
-//                "\t\"isActive\":true,\n" +
-//                "\t\"groupId\":1,\n" +
-//                "}";
-//        out.println(request);
-//        out.flush();
-//
-//        StringBuilder actualResult = new StringBuilder();
-//        actualResult.append(in.readLine()).append("\n");
-//        while (in.ready()) {
-//            actualResult.append(in.readLine()).append("\n");
-//        }
-//        actualResult.setLength(actualResult.length() - 1);
-//        // сначала сравниваем ответы
-//        assertThat(actualResult.toString()).isEqualTo(expectedResult);
-//        // сравниваем результаты
-//        String sqlQueryUser = "SELECT * FROM \"user\" WHERE id=1;";
-//        connection = poolConnection.getConnection();
-//        statement = connection.createStatement();
-//        ResultSet result = statement.executeQuery(sqlQueryUser);
-//        result.next();
-//        soft.assertThat(user)
-//                .hasFieldOrPropertyWithValue("password", result.getString("password"))
-//                .hasFieldOrPropertyWithValue("username", result.getString("username"))
-//                .hasFieldOrPropertyWithValue("firstName", result.getString("first_name"))
-//                .hasFieldOrPropertyWithValue("lastName", result.getString("last_name"))
-//                .hasFieldOrPropertyWithValue("email", result.getString("email"))
-//                .hasFieldOrPropertyWithValue("lastLogin", result.getTimestamp("last_login").toLocalDateTime().toLocalDate())
-//                .hasFieldOrPropertyWithValue("dateJoined", result.getTimestamp("date_joined").toLocalDateTime().toLocalDate())
-//                .hasFieldOrPropertyWithValue("isSuperuser", result.getBoolean("is_superuser"))
-//                .hasFieldOrPropertyWithValue("isStaff", result.getBoolean("is_staff"))
-//                .hasFieldOrPropertyWithValue("isActive", result.getBoolean("is_active"))
-//                .hasFieldOrPropertyWithValue("groupId", result.getInt("group_id"));
-//        soft.assertAll();
-//    }
+    @Test
+    void buildResponsePUTMethod() throws IOException, SQLException {
+        SoftAssertions soft = new SoftAssertions();
+        Connection connection = this.poolConnection.getConnection();
+        Statement statement = connection.createStatement();
+        String sqlInsertSource = "INSERT INTO source (title, url) " +
+                "VALUES ('source1', 'url');";
+        statement.executeUpdate(sqlInsertSource);
+        Source source = new Source("Яндекс ДЗЕН","https://zen.yandex.ru/");
 
-//    @Test
-//    void buildResponseDELETEMethod() throws SQLException, IOException {
-//        Connection connection = this.poolConnection.getConnection();
-//        Statement statement = connection.createStatement();
-//        String sqlInsertUser = String.format("INSERT INTO \"user\" (password, username, first_name, last_name, email, " +
-//                        "last_login, date_joined, is_superuser, is_staff, is_active, group_id) VALUES ('%s', '%s', '%s', '%s', " +
-//                        "'%s', '%s', '%s', %s, %s, %s, %s);", "password1", "username1", "firstname1", "lastname1", "email1@mail.ru",
-//                Timestamp.valueOf(lastLogin.atStartOfDay()), Timestamp.valueOf(dateJoined.atStartOfDay()), true, true, true, 1);
-//        statement.executeUpdate(sqlInsertUser);
-//
-//        clientSocket = new Socket("127.0.0.1", 5000);
-//        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-//        out = new PrintWriter(new PrintWriter(clientSocket.getOutputStream(), true));
-//        String expectedResult = "" +
-//                "HTTP/1.1 204 Нет данных\n" +
-//                "Cache-Control: no-store, no-cache, must-revalidate\n" +
-//                "Pragma: no-cache\n";
-//
-//        String request = "" +
-//                "DELETE /user/1/ HTTP/1.1\n" +
-//                "Accept: application/json, */*; q=0.01\n" +
-//                "Content-Type: application/json\n" +
-//                "Host: 127.0.0.1:5000\n" +
-//                "UnitTest: true\n" +
-//                "UrlPostgres: " + this.container.getJdbcUrl() + "\n" +
-//                "UserPostgres: " + this.container.getUsername() + "\n" +
-//                "PasswordPostgres: " + this.container.getPassword() + "\n";
-//        out.println(request);
-//        out.flush();
-//
-//        StringBuilder actualResult = new StringBuilder();
-//        actualResult.append(in.readLine()).append("\n");
-//        while (in.ready()) {
-//            actualResult.append(in.readLine()).append("\n");
-//        }
-//        actualResult.setLength(actualResult.length() - 1);
-//        // сначала сравниваем ответы
-//        assertThat(actualResult.toString()).isEqualTo(expectedResult);
-//        // сравниваем результаты из таблиц
-//        String sqlQueryUser = "SELECT * FROM \"user\" WHERE id=1;";
-//        ResultSet result = statement.executeQuery(sqlQueryUser);
-//        assertThat(result.next()).isFalse().as("Не удален пользователь по запросу");
-//    }
+        clientSocket = new Socket("127.0.0.1", 5000);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        out = new PrintWriter(new PrintWriter(clientSocket.getOutputStream(), true));
+        String expectedResult = "" +
+                "HTTP/1.1 204 Нет данных\n" +
+                "Cache-Control: no-store, no-cache, must-revalidate\n" +
+                "Pragma: no-cache\n";
+
+        String request = "" +
+                "PUT /source/1/ HTTP/1.1\n" +
+                "Accept: application/json, */*; q=0.01\n" +
+                "Content-Type: application/json\n" +
+                "Host: 127.0.0.1:5000\n" +
+                "UnitTest: true\n" +
+                "UrlPostgres: " + this.container.getJdbcUrl() + "\n" +
+                "UserPostgres: " + this.container.getUsername() + "\n" +
+                "PasswordPostgres: " + this.container.getPassword() + "\n" +
+                "\n" +
+                "{\n" +
+                "\t\"id\":1,\n" +
+                "\t\"title\":\"Яндекс ДЗЕН\",\n" +
+                "\t\"url\":\"https://zen.yandex.ru/\",\n" +
+                "}";
+        out.println(request);
+        out.flush();
+
+        StringBuilder actualResult = new StringBuilder();
+        actualResult.append(in.readLine()).append("\n");
+        while (in.ready()) {
+            actualResult.append(in.readLine()).append("\n");
+        }
+        actualResult.setLength(actualResult.length() - 1);
+        // сначала сравниваем ответы
+        assertThat(actualResult.toString()).isEqualTo(expectedResult);
+        // сравниваем результаты
+        String sqlQuerySource = "SELECT * FROM source WHERE id=1;";
+        connection = poolConnection.getConnection();
+        statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(sqlQuerySource);
+        result.next();
+        soft.assertThat(source)
+                .hasFieldOrPropertyWithValue("title", result.getString("title"))
+                .hasFieldOrPropertyWithValue("url", result.getString("url"));
+        soft.assertAll();
+    }
+
+    @Test
+    void buildResponseDELETEMethod() throws SQLException, IOException {
+        Connection connection = this.poolConnection.getConnection();
+        Statement statement = connection.createStatement();
+        String sqlInsertSource = "INSERT INTO source (title, url) " +
+                "VALUES ('source1', 'url');";
+        statement.executeUpdate(sqlInsertSource);
+
+        clientSocket = new Socket("127.0.0.1", 5000);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        out = new PrintWriter(new PrintWriter(clientSocket.getOutputStream(), true));
+        String expectedResult = "" +
+                "HTTP/1.1 204 Нет данных\n" +
+                "Cache-Control: no-store, no-cache, must-revalidate\n" +
+                "Pragma: no-cache\n";
+
+        String request = "" +
+                "DELETE /source/1/ HTTP/1.1\n" +
+                "Accept: application/json, */*; q=0.01\n" +
+                "Content-Type: application/json\n" +
+                "Host: 127.0.0.1:5000\n" +
+                "UnitTest: true\n" +
+                "UrlPostgres: " + this.container.getJdbcUrl() + "\n" +
+                "UserPostgres: " + this.container.getUsername() + "\n" +
+                "PasswordPostgres: " + this.container.getPassword() + "\n";
+        out.println(request);
+        out.flush();
+
+        StringBuilder actualResult = new StringBuilder();
+        actualResult.append(in.readLine()).append("\n");
+        while (in.ready()) {
+            actualResult.append(in.readLine()).append("\n");
+        }
+        actualResult.setLength(actualResult.length() - 1);
+        // сначала сравниваем ответы
+        assertThat(actualResult.toString()).isEqualTo(expectedResult);
+        // сравниваем результаты из таблиц
+        String sqlQuerySource = "SELECT * FROM source WHERE id=1;";
+        ResultSet result = statement.executeQuery(sqlQuerySource);
+        assertThat(result.next()).isFalse().as("Не удален источник по запросу");
+    }
 }
