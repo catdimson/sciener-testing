15078a808be3be9ea8ca3f85fb5f700dbea8c67c
Dmitrj Kotik <k-work31@yandex.ru>
2021-05-25 23:25:35 +0300

    closed #16. Add spring-boot-starter-web and spring-boot-starter-test. Connection spring. Created BankService and tests for him.

--- src/main/java/news/service/bank/BankService.java
+++ src/main/java/news/service/bank/BankService.java
@@ -1,23 +1,27 @@
 package news.service.bank;
 
 import news.model.bank.CourseValute;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+@Service
 public class BankService {
     private final RestTemplate restTemplate;
     private final String URL_BANK;
 
-    public BankService(RestTemplateBuilder restTemplateBuilder, String url) {
+    public BankService(RestTemplateBuilder restTemplateBuilder, @Value("${bankurl}") String URL_BANK) {
         this.restTemplate = restTemplateBuilder.build();
-        this.URL_BANK = url; // "https://www.cbr-xml-daily.ru/daily_json.js";
+        this.URL_BANK = URL_BANK;
     }
 
     public CourseValute getCourseValute() {
@@ -28,7 +32,12 @@ public class BankService {
         converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
         messageConverters.add(converter);
         restTemplate.setMessageConverters(messageConverters);
-        courseValute = restTemplate.getForObject(URL_BANK, CourseValute.class);
+
+        try {
+            courseValute = restTemplate.getForObject(URL_BANK, CourseValute.class);
+        } catch (RestClientException e) {
+            courseValute = null;
+        }
 
         return courseValute;
     }
