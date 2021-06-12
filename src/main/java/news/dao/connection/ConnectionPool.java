add212668325b54b9f9efaed69cbc668ea8fe9fd
Dmitrj Kotik <k-work31@yandex.ru>
2021-06-13 02:20:41 +0300

    #15 deleted custom ConnectionPool and custom controllers

--- src/main/java/news/dao/connection/ConnectionPool.java
+++
@@ -1,9 +0,0 @@
-package news.dao.connection;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-@Deprecated
-public interface ConnectionPool {
-    Connection getConnection() throws SQLException;
-}
